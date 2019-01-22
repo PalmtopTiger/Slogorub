4130bc34f3288a4e415b0ce463a5b08d143e0309
Andrey Efremov <duxus@yandex.ru>
2019-01-22 21:41:50 +0700

    Fix warnings

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -10,11 +10,11 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    
+
 private slots:
     void on_btExit_clicked();
 
